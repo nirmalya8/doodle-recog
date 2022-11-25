934902cc27f3bd5912cb4e46f656aa23d2b31b3d
nirmalya8 <nirmalya14misra@gmail.com>
2022-11-25 09:44:47 +0530

    Refactored app.py

--- classifier/app.py
+++ classifier/app.py
@@ -6,14 +6,18 @@ model = LeNet()
 model.load_state_dict(torch.load("./Models/lenet2.pt"))
 from model import LeNet 
 def predict(img):
-    x = torch.tensor(img, dtype=torch.float32).unsqueeze(0).unsqueeze(0) / 255.
-    print(x.shape)
-    with torch.no_grad():
-        out = model(x)
-    probabilities = torch.nn.functional.softmax(out[0], dim=0)
-    values, indices = torch.topk(probabilities, 5)
-    confidences = {classes[i]: v.item() for i, v in zip(indices, values)}
-    return confidences
+    try:
+        x = torch.tensor(img, dtype=torch.float32).unsqueeze(0).unsqueeze(0) / 255.
+        # print(x.shape)
+        with torch.no_grad():
+            out = model(x)
+        probabilities = torch.nn.functional.softmax(out[0], dim=0)
+        values, indices = torch.topk(probabilities, 5)
+        confidences = {classes[i]: v.item() for i, v in zip(indices, values)}
+        return confidences
+    except TypeError:
+        pass
+    
 
 import gradio as gr
 
